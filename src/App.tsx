20c42acf8e67e840e73b79f90a05f1838cbe1ecd
Royweru <royezi17@gmail.com>
2024-03-01 09:19:47 +0300

    new file:   .eslintrc.cjs 	new file:   .gitignore 	new file:   components.json 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   postcss.config.js 	new file:   public/vite.svg 	new file:   src/App.tsx 	new file:   src/assets/react.svg 	new file:   src/components/navigation/navbar.tsx 	new file:   src/components/theme-provider.tsx 	new file:   src/index.css 	new file:   src/lib/utils.ts 	new file:   src/main.tsx 	new file:   src/vite-env.d.ts 	new file:   tailwind.config.js 	new file:   tsconfig.json 	new file:   tsconfig.node.json 	new file:   vite.config.ts

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+
+
+function App() {
+  
+  return (
+    <>
+     <div className=' text-4xl text-red-200 font-bold text-center w-full'>
+      CARPOOL OPENING
+     </div>
+    </>
+  )
+}
+
+export default App
